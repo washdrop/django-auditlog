3d4e4b96c980ad28c525bcdddec5ce0f80ee636d
Perry Roper <git@accounts.perry.io>
2019-12-08 14:22:18 +0700

    Remove more jsonfield references.

--- src/auditlog/apps.py
+++ src/auditlog/apps.py
@@ -1,12 +1,6 @@
-from __future__ import unicode_literals
-
 from django.apps import AppConfig
 
 
 class AuditlogConfig(AppConfig):
     name = 'auditlog'
     verbose_name = "Audit log"
-
-    def ready(self):
-        import jsonfield_compat
-        jsonfield_compat.register_app(self)
